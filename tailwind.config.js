a4aea9fef60630bcb93080e5f4f0d280dbae0afb
erichvorster <erichvorster1@gmail.com>
2022-03-06 14:43:04 +0200

    2nd commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./**/*.{html,js,css}"
+    
+  ],
+
+  theme: {
+    extend:{
+      colors: {
+        'body': '#17171F',
+        'selected-text' : '#A3A3FF',
+        'theme':'#3f3fff',
+        'nav':'#404053',
+        'secondary':'#9191A4',
+        'badge': '#3f3f51',
+        'input-border':'#565666',
+        'input':'#2a2a35'
+      },
+      fontFamily: {
+        'poppins' : ["'poppins'", 'sans-serif']
+      }
+    },
+  },
+
+}
